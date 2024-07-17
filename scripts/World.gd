6495f96a9a15b68dee3cddb091ec033b2eb8ebdc
Axel Gomez <axgomezm@unal.edu.co>
2024-07-17 15:21:11 -0500

    Mapa con nuevos tiles, y con objetos(trees, grass, rocks)

--- scripts/World.gd
+++ scripts/World.gd
@@ -9,13 +9,15 @@ var height = 1000
 var matrix
 
 var tileTypes = {
-	"deepOcean": {"position": Vector2i(28, 3), "tileId": 0},
-	"ocean": {"position": Vector2i(21, 3), "tileId": 0},
-	"sand": {"position": Vector2i(15, 3), "tileId": 0},
-	"land": {"position": Vector2i(11, 3), "tileId": 0},
-	"deepland": {"position": Vector2i(3, 3), "tileId": 0},
-	"rock": {"position": Vector2i(0, 0), "tileId": 1},
-	"tree": {"position": Vector2i(0, 4), "tileId": 1},}
+	"ocean": {"position": Vector2i(2, 5), "tileId": 0},
+	"sand": {"position": Vector2i(4, 5), "tileId": 0},
+	"land": {"position": Vector2i(3, 5), "tileId": 0},
+	"darkLand": {"position": Vector2i(3, 0), "tileId": 0},}
+	
+var tileObjects = {
+	"rock": {"position": Vector2i(0, 0), "tileId": 0},
+	"tree": {"position": Vector2i(0, 2), "tileId": 0},
+	"grass": {"position": Vector2i(0, 4), "tileId": 0},}
 
 func _ready():
 	tilemap = $TileMap
@@ -23,6 +25,7 @@ func _ready():
 	noise.seed = randi()
 	createMatrix()
 	createMap()
+	tilemap.set_cell(1,Vector2(5,5) ,0, Vector2i(0, 0))
 	generateTerrain()
 
 func _process(_delta):
@@ -41,54 +44,69 @@ func createMap():
 		for y in range(height):
 			if matrix[x][y]!= null:
 				continue
+			var zone : String
 			var value = noise.get_noise_2d(x, y)
-			if (value >= -0.1 and value <= 1) and randf() < 0.001:  
+			zone = getZone(value)
+			matrix[x][y]={"tile":zone,"object":null,"done": false}
+			if (value >= -0.4 and value <= 1) and randf() < 0.05:  
 				if areaAvailable(x, y, value):
-						placeObject(x, y, "tree")
+						#placeObject(x, y, zone, "tree")
+						matrix[x][y] = {"tile":zone,"object": "tree", "done": false}
 						continue
-			elif (value > -0.3 and value <= 1) and randf() < 0.001:
+			elif (value > -0.1 and value <= 1) and randf() < 0.05:
 				if areaAvailable(x, y, value):
-					placeObject(x, y, "rock")
+					#placeObject(x, y, zone, "rock")
+					matrix[x][y] = {"tile":zone,"object": "rock", "done": false}
 					continue
-			if value >= -1 and value < -0.5:
-				matrix[x][y] = {"tile":"deepOcean","done": false}
-			elif value >= -0.5 and value <= -0.3:
-				matrix[x][y] = {"tile":"ocean","done": false}
-			elif value > -0.3 and value < -0.1:
-				matrix[x][y] = {"tile":"sand","done": false}
-			elif value >= -0.1 and value < 0.3:
-				matrix[x][y] = {"tile":"land","done": false}
-			elif value >= 0.3 and value <= 1:
-				matrix[x][y] = {"tile":"deepland","done": false}
-				
+			elif (value > -0.1 and value <= 1) and randf() < 0.05:
+				if areaAvailable(x, y, value):
+					#placeObject(x, y, zone, "rock")
+					matrix[x][y] = {"tile":zone,"object": "grass", "done": false}
+					continue
+					
+func getZone(value):
+	var zone : String
+	if value >= -1 and value < -0.4:
+		zone = "ocean"
+	elif value >= -0.4 and value < -0.1:
+		zone = "sand"
+	elif value >= -0.1 and value <= 1:
+		var probLand = 1 + value
+		if randf_range(-0.4, probLand) > 0.7:
+			zone = "land"
+		else:
+			zone = "darkLand"
+	return zone
+		
 func areaAvailable(startX, startY, value):
-	for x in range(startX, startX + 4):
-		for y in range(startY, startY + 4):
+	for x in range(startX-2, startX + 2):
+		for y in range(startY-2, startY + 2):
 			if x >= width or y >= height:
 				return false 
-			if  noise.get_noise_2d(x,y) < -0.3:
+			if  noise.get_noise_2d(x,y) < -0.1:
 				return false  
 			if matrix[x][y]!= null:
-				return false  
+				if matrix[x][y]["object"]!= null:
+					return false  
 	return true
 
-func placeObject(startX, startY, objectType):
-	for x in range(startX, startX + 4):
-		for y in range(startY, startY + 4):
-			matrix[x][y] = {"tile": objectType, "done": false}				
+func placeObject(startX, startY, zone, objectType):
+	for x in range(startX, startX + 2):
+		for y in range(startY, startY + 2):
+			matrix[x][y] = {"tile":zone,"object": objectType, "done": false}				
 			
 func generateTerrain():
 	for x in range(width):
 		for y in range(height):
-			if matrix [x][y]["done"]== true:
+			var cell = matrix [x][y]
+			if cell["done"]== true:
 				continue
-			var value = matrix [x][y]["tile"]
-			if value == "tree" or value == "rock":
-				for i in range(4):
-					for j in range(4):
-						var vector = tileTypes[value]["position"]
-						tilemap.set_cell(0,Vector2(x+i,y+j) ,tileTypes[value]["tileId"], Vector2i(vector.x+i,vector.y+j ) )
-						matrix [x+i][y+j]["done"] = true
-				continue
-			tilemap.set_cell(0,Vector2(x,y) ,tileTypes[value]["tileId"], tileTypes[value]["position"])
-			matrix [x][y]["done"] = true
+			tilemap.set_cell(0,Vector2(x,y) ,tileTypes[cell["tile"]]["tileId"], tileTypes[cell["tile"]]["position"])
+			#matrix [x][y]["done"] = true
+			if cell["object"] != null:
+				tilemap.set_cell(1,Vector2(x,y) ,tileObjects[cell["object"]]["tileId"], tileObjects[cell["object"]]["position"])
+				#for i in range(2):
+					#for j in range(2):
+						#var vector = tileTypes[cell["tile"]]["position"]
+						#tilemap.set_cell(0,Vector2(x+i,y+j) ,tileTypes[cell["tile"]]["tileId"], Vector2i(vector.x+i,vector.y+j ))
+						#matrix [x+i][y+j]["done"] = true
