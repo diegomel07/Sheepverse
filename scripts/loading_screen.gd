9e1104961a17c6673126a3af56b9e91bd555334c
axgomezm <49238382+axgomezm@users.noreply.github.com>
2024-07-19 13:59:12 -0500

    jejej

--- scripts/loading_screen.gd
+++ scripts/loading_screen.gd
@@ -12,7 +12,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	status = ResourceLoader.load_threaded_get_status(scene, progress)
-	print(progress)
 	if status == ResourceLoader.THREAD_LOAD_LOADED:
 		var newScene = ResourceLoader.load_threaded_get(scene)
 		get_tree().change_scene_to_packed(newScene)
